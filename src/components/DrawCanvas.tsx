e220d4eabb95e757340d75c7aa374cd234dc3a1c
Oyewo Temidayo <oyewodayo@gmail.com>
2024-07-19 09:20:44 +0100

    Customization of drawer panel options

--- src/components/DrawCanvas.tsx
+++ src/components/DrawCanvas.tsx
@@ -8,9 +8,10 @@ import { TbBackground } from 'react-icons/tb';
 
 interface DrawCanvasProps{
     initialColor?: string;
+    initialBackgroundColor?:string;
     initialLineWidth?: number;
 }
-const DrawCanvas = ({initialColor='black',initialLineWidth=4}:DrawCanvasProps) => {
+const DrawCanvas = ({initialColor='black',initialBackgroundColor="#FFFFFF",initialLineWidth=4}:DrawCanvasProps) => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const contextRef = useRef<CanvasRenderingContext2D | null>(null);
     const pencilButtonRef = useRef<HTMLButtonElement | null>(null);
@@ -18,17 +19,31 @@ const DrawCanvas = ({initialColor='black',initialLineWidth=4}:DrawCanvasProps) =
     const [isErasing, setIsErasing] = useState(false);
     const [activeButton, setActiveButton] = useState<string | null>(null);
     const [penColor, setPenColor] = useState<string>(initialColor);
+    const [backgroundColor, setBackgroundColor] = useState<string>(initialBackgroundColor);
     const [penSize, setPenSize] = useState(initialLineWidth);
     const [showControls, setShowControls] = useState(false);
+    const [backgroundControl, setBackgroundControl] = useState(false);
     const [controlsPosition, setControlsPosition] = useState({ top: 0, left: 0 });
-    const [drawerPanelOption, setDrawerPanelOption] = useState<string>("horizontal");
+    const [drawerPanelOption, setDrawerPanelOption] = useState<string>("vertical");
+    const [showSettingsDropdown, setShowSettingsDropdown] = useState(false);
 
     const handleButtonClick = (buttonName:string) => {
         setActiveButton(buttonName);
+        if (buttonName !="settings") {
+            setShowSettingsDropdown(false);
+        }
+
+        if (buttonName !="background") {
+            setBackgroundControl(false);
+            
+        }
       };
 
 
     const toggleControls = () => {
+        setShowControls(!showControls);
+    };
+    const toggleBackroundControls = () => {
         if (pencilButtonRef.current) {
             const rect = pencilButtonRef.current.getBoundingClientRect();
             setControlsPosition({
@@ -36,8 +51,16 @@ const DrawCanvas = ({initialColor='black',initialLineWidth=4}:DrawCanvasProps) =
                 left: rect.left - 120, // Adjust this value to position the controls correctly
             });
         }
-        setShowControls(!showControls);
+        setBackgroundControl(!backgroundControl);
+        setShowControls(false)
     };
+
+
+
+    const handleBackgroundColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+        setBackgroundColor(e.target.value);
+    }
 
     const handleColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPenColor(e.target.value);
@@ -45,6 +68,7 @@ const DrawCanvas = ({initialColor='black',initialLineWidth=4}:DrawCanvasProps) =
             contextRef.current.strokeStyle = e.target.value;
         }
     }
+
 
     const handleSizeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newSize = parseInt(e.target.value);
@@ -140,14 +164,18 @@ const DrawCanvas = ({initialColor='black',initialLineWidth=4}:DrawCanvasProps) =
             contextRef.current?.moveTo(offsetX, offsetY);
         }
 
+        
         setIsDrawing(true);
-        setShowControls(false);
+        setShowSettingsDropdown(false);
+        setBackgroundControl(false);
+        setShowControls(false)
 
        
     }
 
     const draw = useCallback((event:React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>)=>{
         event.preventDefault();
+       
         if (!isDrawing) return;
 
          const { offsetX, offsetY } = getCoordinates(event); 
@@ -189,11 +217,14 @@ const DrawCanvas = ({initialColor='black',initialLineWidth=4}:DrawCanvasProps) =
             setIsDrawing(false);
         }
     }
+    const toggleSettingsDropdown = () => {
+        setShowSettingsDropdown(!showSettingsDropdown);
+    }
 
   return (
-    <div className='flex h-screen relative'>
+    <div className={`flex h-screen relative`}  style={{ backgroundColor: backgroundColor }}>
         <canvas 
-            className="w-[97%] "
+            className={`w-[97%] `}
             ref={canvasRef}
             onMouseDown={startDrawing}
             onMouseMove={draw}
@@ -205,62 +236,96 @@ const DrawCanvas = ({initialColor='black',initialLineWidth=4}:DrawCanvasProps) =
         >
     
         </canvas>
-        <Draggable>
+        <Draggable key={"settings"} cancel=".non-draggable">
             <div className={`${drawerPanelOption==="vertical"?'h-[80%] flex flex-col justify-between w-10 place-items-center text-center':'w-[60%] h-[12] flex justify-between place-items-center'}  rounded border absolute right-0 m-2 bg-white`}>
                 <div className={`flex ${drawerPanelOption==="vertical"?'flex-col  my-3':'flex-row '} gap-3 py-2 text-center items-center`}>
                     <button className={`cursor-move p-2 ${drawerPanelOption==="vertical"?'-mt-3':'ml-2'} rounded text-lg hover:bg-blue-500 hover:text-white`}>
                         <GrDrag/>
                     </button>
-
-                    <button onClick={()=>{handleButtonClick('background')}} className={`cursor-pointer p-2 rounded text-lg hover:bg-blue-500 ${activeButton=="background" ? 'bg-blue-200' : ''} hover:text-white`}>
-                        <TbBackground/>
-                    </button>
+                    <div className="relative">
+                        <button onClick={()=>{ toggleBackroundControls();handleButtonClick('background')}} className={`cursor-pointer p-2 rounded text-lg hover:bg-blue-500 ${activeButton=="background" ? 'bg-blue-200' : ''} hover:text-white`}>
+                            <TbBackground/>
+                        </button>
+                        {backgroundControl && (
+                            <div className={`absolute ${drawerPanelOption==="vertical"?'right-10 -top-0.5':'-left-11 top-11'}  p-1 rounded bg-blue-200 w-32`}>
+                                <div>
+                                <small className='font-bold text-xs'>Background Color</small>
+                                    <input 
+                                        type="color" 
+                                        name="background_color" 
+                                        id="background_color" 
+                                        value={backgroundColor}
+                                        onChange={handleBackgroundColorChange}
+                                        className="non-draggable w-full rounded outline-none bg-none fill-none"
+                                    />
+                                </div>                                
+                            </div>
+                        )}
+                    </div>
 
                     <div className='relative'>
-                        <button 
-                        
+                        <button                         
                             ref={pencilButtonRef}
                             className={`cursor-pointer text-lg align-middle text-center rounded hover:bg-blue-500 hover:text-white p-2 ${activeButton==="pencil" ? 'bg-blue-200' : ''} `}
-                            onClick={() => { setToDraw(); toggleControls(); handleButtonClick("pencil") }}
+                            onClick={() => { setToDraw(); toggleControls();setBackgroundControl(false);setShowSettingsDropdown(false); handleButtonClick("pencil") }}
                             aria-label="Pencil tool"
                         >
                         <IoPencil />
                         </button>
+                        {activeButton=="pencil" && (
+                            <div className='non-draggable -mt-1.5 h-2.5 cursor-pointer '> 
+                                <input 
+                                    type="range" 
+                                    min={1} 
+                                    max={20}
+                                    name="pen_size" 
+                                    id="pen_size" 
+                                    value={penSize}
+                                    onChange={handleSizeChange}
+                                    className={`non-draggable custom-range ${drawerPanelOption==="vertical"?'w-full':'w-9'} `}
+                                />
+                            </div>
+                        )}
                         {showControls && (
-                            <div className={`absolute ${drawerPanelOption==="vertical"?'-left-32 -top-4':'-left-11 top-11'}  p-1 rounded bg-slate-400 w-32`}>
-                                <div>
+                            <div className={`absolute ${drawerPanelOption==="vertical"?'right-10 -top-0.5 w-14':'top-[3rem] -right-0.5 w-10'} p-1 rounded bg-blue-200`}>
+                                <div className='text-left'>
                                     <input 
                                         type="color" 
                                         name="pen_color" 
                                         id="pen_color" 
                                         value={penColor}
                                         onChange={handleColorChange}
-                                        className="w-full rounded outline-none bg-none fill-none"
+                                        className={`non-draggable rounded outline-none bg-none ${drawerPanelOption==="vertical"?'h-6.5 w-full':'h-6.5 w-full'}`}
                                     />
                                 </div>
-                                <div className='flex mt-2'>
-                                    <input 
-                                        type="range" 
-                                        min={1} 
-                                        max={20}
-                                        name="pen_size" 
-                                        id="pen_size" 
-                                        value={penSize}
-                                        onChange={handleSizeChange}
-                                        className="w-full"
-                                    />
-                                    <span className='text-sm'>{penSize}</span>
-                                </div>
+                                <div className='text-[8px] font-bold'>Size {penSize}</div>
                             </div>
                         )}
                     </div>
+                    <div>
                     <button 
                         className={`cursor-pointer text-lg p-2 rounded  hover:bg-blue-500 hover:text-white ${activeButton=="eraser" ? 'bg-blue-200' : ''}`}
-                        onClick={() => { setToErase(); setShowControls(false); handleButtonClick("eraser") }}
+                        onClick={() => { setToErase(); setShowControls(false);setShowSettingsDropdown(false); handleButtonClick("eraser") }}
                         aria-label="Eraser tool"
                     >
                         <BsEraser />
                     </button>
+                    {activeButton=="eraser" && (
+                        <div className='non-draggable -mt-1.5 h-2.5 cursor-pointer '> 
+                         <input 
+                            type="range" 
+                            min={1} 
+                            max={20}
+                            name="pen_size" 
+                            id="pen_size" 
+                            value={penSize}
+                            onChange={handleSizeChange}
+                            className={`non-draggable custom-range ${drawerPanelOption==="vertical"?'w-full':'w-9'} `}
+                        />
+                        </div>
+                    )}
+                    </div>
+
                     <button 
                         className={`cursor-pointer text-lg p-2 rounded hover:bg-blue-500 ${activeButton=="clear_canva" ? 'bg-blue-200' : ''} hover:text-white`}
                         onClick={() => { clearCanvas(); handleButtonClick("clear_canva") }}
@@ -269,10 +334,21 @@ const DrawCanvas = ({initialColor='black',initialLineWidth=4}:DrawCanvasProps) =
                         <BsFillEraserFill />
                     </button>
                 </div>
-                <div>
-                    <button onClick={()=>{handleButtonClick("settings")}} className={`${activeButton=="settings" ? 'bg-blue-200' : ''} ${drawerPanelOption==="vertical"?'mb-2':'mr-3'} left-0 cursor-pointer text-lg p-2 rounded hover:bg-blue-500`}>
-                        <IoSettings/>
+                <div className='relative'>
+                    <button onClick={() => { toggleSettingsDropdown(); handleButtonClick("settings") }} className={`${activeButton === "settings" ? 'bg-blue-200' : ''} ${drawerPanelOption === "vertical" ? 'mb-2' : 'mr-3'} left-0 cursor-pointer text-lg p-2 rounded hover:bg-blue-500`}>
+                        <IoSettings />
                     </button>
+                    {showSettingsDropdown && (
+                        
+                        <div className={`non-draggable absolute ${drawerPanelOption==="vertical"?'right-10 -top-10  w-48':'w-[200px] right-0'} mt-2 text-center  rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5`}>
+                            <div className='font-semibold p-2 border-b-1'>Settings </div>
+                            <hr />
+                            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                <button className="w-[100%] block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Option 1</button>
+                                <button className="w-[100%] block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Option 2</button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </Draggable>
